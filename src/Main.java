d8ebbbf53e8fc3aaf7a4a465dcbdd0c718fd8440
mismusen <michelle_bundgaard@hotmail.com>
2018-04-18 14:07:43 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
         System.out.println("Hello");
+        System.out.println("Again");
     }
 
     @Override
